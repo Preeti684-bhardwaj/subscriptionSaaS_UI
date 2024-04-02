8ade57b9fc295e408f6692ef18ae9ba54940365d
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-04-02 16:12:02 +0530

    frontend 02

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Pricing from './component/Pricing';
 function App() {
   return (
     <div className="App">
-      <div className='left-circle'></div>
-      <div className='right-circle'></div>
-      <div className='mid-circle'></div>
-      <div className='bottom-circle'></div>
     <Pricing/>
-
     </div>
   );
 }
