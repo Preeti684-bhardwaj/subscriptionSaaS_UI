1fc13e1a3b4133deb93240cd7508e43eab8bf56b
Preeti Bhardwaj <bhardwajpreeti@gmail.com>
2024-05-09 12:29:42 +0530

    index.html changed seo added

--- src/component/PayButton.jsx
+++ src/component/PayButton.jsx
@@ -25,7 +25,7 @@ const PayButton = ({ planPrice, features, planName, description }) => {
     }
 
     // Both accessToken and userId are available, proceed with creating checkout session
-    axios.post("https://subscriptionplan-server.onrender.com/create-checkout-session", {
+    axios.post("http://localhost:4242/create-checkout-session", {
       planPrice,
       features,
       planName,
